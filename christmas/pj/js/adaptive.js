9fd71bd5f7bafeceae7ea8061b103e6f95a03c04
Arron <arron.laihongwei@gmail.com>
2017-02-18 21:13:27 +0800

    adaptive js
    !! background-size after background

--- christmas/pj/js/adaptive.js
+++ christmas/pj/js/adaptive.js
@@ -1,18 +1,25 @@
 (function(){
+  var docEl = document.documentElement;
   function resize(){
     var docWidth = document.documentElement.clientWidth || document.body.clientWidth;
+    console.log(docWidth);
     docEl.style.fontSize = 20 * docWidth / 320 + "px";
-  }
 
-  var pages = document.getElementsByClassName("page");
-  for (var i = 0; i < pages.length; i++) {
-    pages[i].style.width = pages[i].clientWidth + "px";
-    
-    console.log(pages[i].style.width);
-    console.log(pages[i].clientWidth);
-    // console.log(pages[i].clientWidth * 991 / 1441);
-    pages[i].style.height = pages[i].clientWidth * 991 / 1441 + "px";
-    console.log(pages[i].clientHeight);
-    console.log(pages[i].style.height);
+    var pages = document.getElementsByClassName("page");
+    for (var i = 0; i < pages.length; i++) {
+      // pages[i].style.width = pages[i].clientWidth + "px";
+      //
+      // console.log(pages[i].style.width);
+      // console.log(pages[i].clientWidth);
+      // console.log(pages[i].clientWidth * 991 / 1441);
+      pages[i].style.height = pages[i].clientWidth * 991 / 1441 + "px";
+      // console.log(pages[i].clientHeight);
+      // console.log(pages[i].style.height);
+    }
+
   }
+  window.addEventListener('resize', resize, false);
+  resize();
+
+
 })();
