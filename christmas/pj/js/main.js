231e54009ba20e09dd35e9aa16ce6130d817b9bc
Arron <arron.laihongwei@gmail.com>
2017-02-18 22:56:20 +0800

    scene change

--- christmas/pj/js/main.js
+++ christmas/pj/js/main.js
@@ -1,7 +1,10 @@
-
+  var p1 = document.getElementById("p1");
+  var p3 = document.getElementById("p3");
   function changeScene() {
-    var p1 = document.getElementById("p1");
-    var p2 = document.getElementById("p2");
     p1.setAttribute('class', 'page effectOut');
     // p2.setAttribute('class', 'page effectIn');
+  }
+
+  function changeBack() {
+    p3.setAttribute('class', 'page effectIn');
   }
